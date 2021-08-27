bb814959eb7c3f60fd6dbd03b93ebf6241f1ea9e
Lingga Adi Atmadja <linggaadiatmdj@gmail.com>
2021-08-27 09:40:32 +0700

    AxisAlignment props example

--- wallet_apps/lib/main.dart
+++ wallet_apps/lib/main.dart
@@ -10,40 +10,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("E-Walley"),
+          backgroundColor: Colors.red,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.mail), onPressed: () {})
+          ],
         ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("E-Walley"),
-              backgroundColor: Colors.red,
-              actions: <Widget>[
-                IconButton(icon: Icon(Icons.mail), onPressed: () {})
-              ],
-            ),
-            body: SafeArea(
-                child: Container(
-                    // color: Colors.red,
-                    margin: EdgeInsets.only(left: 25.0, right: 25.0),
-                    padding: EdgeInsets.only(top: 50.0),
-                    child: Column(
-                      children: [
-                        Image(
-                          image: AssetImage("assets/images/ic_mail.png"),
-                          height: 250,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: Text(
-                            "Your Balance Has Been Added!",
-                            style: header,
-                            textAlign: TextAlign.center,
-                          ),
-                        )
-                      ],
-                    )))));
+        body: SafeArea(
+          child: Container(
+            // color: Colors.red,
+            // margin: EdgeInsets.only(left: 25.0, right: 25.0),
+            // padding: EdgeInsets.only(top: 50.0),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage("assets/images/ic_mail.png"),
+                    height: 250,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 5.0),
+                    child: Text(
+                      "Your Balance Has Been Added!",
+                      style: header,
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
